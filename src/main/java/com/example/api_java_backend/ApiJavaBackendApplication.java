7c0670d841977487792eeea3a9621d52ba60d340
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-14 02:00:05 -0400

    feat: Created an initial template of the project

---
+++ src/main/java/com/example/api_java_backend/ApiJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaBackendApplication.class, args);
+	}
+
+}
