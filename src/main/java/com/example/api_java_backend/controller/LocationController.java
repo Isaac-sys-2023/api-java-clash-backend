85a9c2112ff71c36f6c73200a17de2920750aba7
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-14 19:44:13 -0400

    feat: Add endpoint to fetch ranking clanwars by locationID

--- src/main/java/com/example/api_java_backend/controller/LocationController.java
+++ src/main/java/com/example/api_java_backend/controller/LocationController.java
@@ -108,6 +108,30 @@ public class LocationController {
         String endpoint = endpointBuilder.toString();        
         return proxyRequest(endpoint);
     }
+    
+    @GetMapping("/{locationId}/rankings/clanwars")
+    public Mono<ResponseEntity<String>> getRankingClanWarsByLocation(@PathVariable String locationId,
+            @RequestParam(required = false) String limit,
+            @RequestParam(required = false) String after,
+            @RequestParam(required = false) String before) {
+        StringBuilder endpointBuilder = new StringBuilder("/locations/" + locationId + "/rankings/clanwars");
+        boolean hasParam = false;
+
+        if (limit != null) {
+            endpointBuilder.append(hasParam ? "&" : "?").append("limit=").append(limit);
+            hasParam = true;
+        }
+        if (after != null) {
+            endpointBuilder.append(hasParam ? "&" : "?").append("after=").append(after);
+            hasParam = true;
+        }
+        if (before != null) {
+            endpointBuilder.append(hasParam ? "&" : "?").append("before=").append(before);
+        }
+        
+        String endpoint = endpointBuilder.toString();
+        return proxyRequest(endpoint);
+    }
 
     // Método para centralizar la obtención de datos y el manejo de errores
     private Mono<ResponseEntity<String>> proxyRequest(String endpoint) {
