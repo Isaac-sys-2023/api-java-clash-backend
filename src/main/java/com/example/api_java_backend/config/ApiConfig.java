2a515da0387f278e983d5816eb011885311000ec
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-07-14 18:40:18 -0400

    feat: Configure WebClient with base URL and API key for Clash Royale API

---
+++ src/main/java/com/example/api_java_backend/config/ApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.api_java_backend.config;
+
+/**
+ *
+ * @author chichimon
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ApiConfig {
+    @Value("${clash.api.key}")
+    private String apiKey;
+
+    @Value("${clash.api.url}")
+    private String apiUrl;
+
+    @Bean
+    public WebClient clashApiClient() {
+        return WebClient.builder()
+                .baseUrl(apiUrl)
+                .defaultHeader("Authorization", "Bearer " + apiKey)
+                .build();
+    }
+}
